2a979f816ef7a8c2cc247749c796e305e959ccd0
Deni Sugiarto <sugiarto.deni@gmail.com>
2022-05-30 15:55:46 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+//list dependences
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const prefix = require("gulp-autoprefixer");
+const minify = require("gulp-clean-css");
+const imagewebp = require("gulp-webp");
+const nunjucksRender = require("gulp-nunjucks-render");
+
+//create function
+//html
+
+function htmlTemplate() {
+  return src("src/templates/pages/*.html")
+    .pipe(nunjucksRender({ path: ["src/templates/components"] }))
+    .pipe(dest("dist"));
+}
+//scss
+function compilescss() {
+  return src("src/scss/*.scss")
+    .pipe(sass())
+    .pipe(prefix("last 2 versions"))
+    .pipe(minify())
+    .pipe(dest("dist/assets/css"));
+}
+
+//web images
+function webpImage() {
+  return src("src/images/*.{jpg, png}").pipe(imagewebp()).pipe(dest("dist/assets/images"));
+}
+
+function images() {
+  return src("src/images/*.webp").pipe(dest("dist/assets/images"));
+}
+
+//svg
+function svgCopy() {
+  return src("src/icons/*.svg").pipe(dest("dist/assets/icons"));
+}
+
+//create watch task
+function watchTask() {
+  watch("src/templates/**/*", htmlTemplate);
+  watch("src/scss/*.scss", compilescss);
+  watch("src/images/*.{jpg, png}", webpImage);
+  watch("src/images/*.webp", images);
+  watch("src/icons/*.svg", svgCopy);
+}
+
+//default gulp
+exports.default = series(htmlTemplate, compilescss, webpImage, images, svgCopy, watchTask);
